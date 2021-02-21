3c9effe7ba96373ba24d8e1334eaac4aaba0be05
oleeriksen <oe@easv.dk>
2021-02-21 18:50:08 +0100

    Initial commit

---
+++ AsyncExercise/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace AsyncExercise
+{
+    public class Client
+    {
+        private readonly Server mServer;
+
+        public Client(Server server)
+        {
+            mServer = server;
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                int number = GetNumber("Enter a number (0 for stop): ");
+
+                if (number == 0) break;
+
+                int[] numbers = mServer.GetNumbers(number, 1, 6);
+
+                Console.WriteLine("Here are the numbers from the server: ");
+
+                foreach (int x in numbers)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+
+        private int GetNumber(String text)
+        {
+            Console.WriteLine(text);
+            return int.Parse(Console.ReadLine());
+        }
+
+
+    }
+}
